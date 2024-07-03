3c909c32a73c50d24133c8968386e44cfae3c7cc
JKiyo7 <157696783+JKiyo7@users.noreply.github.com>
2024-07-03 13:03:15 +0800

    Update DropMaterializa.js

--- DropMaterializa.js
+++ DropMaterializa.js
@@ -1,4 +1,17 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the element for "Materiales" dropdown link
+    var materialesLink = document.getElementById("materiales-link");
+
+    // Define the URL you want to navigate to
+    var materialesURL = "/productos/";  // Replace with your desired URL
+
+    // Add a click event listener to the "Materiales" link
+    materialesLink.addEventListener("click", function() {
+        window.location.href = materialesURL;
+    });
+});
+
+/*$(document).ready(function() {
     // Handle click on "Materiales" link
     $('.navbar-item.has-dropdown > .navbar-link').click(function(e) {
         e.preventDefault(); // Prevent default link behavior
